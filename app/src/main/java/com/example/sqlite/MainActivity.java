ab46504dc46b55d8ed06bb0a0059d519e5745aba
PrinceThakur007 <princepatyal007@gmail.com>
2020-05-01 11:51:56 +0530

    first commit

---
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.sql.DriverManager.println;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    DataBaseHelper myDB;
+    EditText editId, editName, editEmail, editAge;
+    Button buttonAdd, buttonGetData, buttonUpdate, buttonDelete, buttonViewAll;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myDB = new DataBaseHelper(this);
+
+        editId = findViewById(R.id.editText_id);
+        editName = findViewById(R.id.editText_name);
+        editEmail = findViewById(R.id.edit_email);
+        editAge = findViewById(R.id.editage);
+
+
+        buttonAdd = findViewById(R.id.add_data_button);
+        buttonGetData = findViewById(R.id.get_data_button);
+        buttonUpdate = findViewById(R.id.update_data_button);
+        buttonDelete = findViewById(R.id.delete_data_button);
+        buttonViewAll = findViewById(R.id.view_all_data_button);
+
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result = myDB.insertData(editName.getText().toString(), editEmail.getText().toString(), editAge.getText().toString());
+
+                if (result) {
+                    Toast.makeText(MainActivity.this, "Data is inserted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Data is not inserted", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+
+        });
+
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor c = myDB.getData("1");
+
+                String data = null;
+                if (c.moveToNext()) {
+                    data = c.getString(0) + "\n" + c.getString(1) + "\n" + c.getString(2) + "\n" + c.getString(3);
+                }
+
+
+                println(data);
+            }
+        });
+
+
+    }
+
+
+}
